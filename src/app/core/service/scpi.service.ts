e0e6cbca808b7fa0b2ad602c4eb472febc88d2ad
TegHocine <61586802+TegHocine@users.noreply.github.com>
2025-02-11 19:52:41 +0100

    Scrum-6: Affichage des SCPIs

---
+++ src/app/core/service/scpi.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject, tap } from 'rxjs';
+import { ScpiModel } from '../model/scpi.model';
+
+type Scpis = ScpiModel[];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScpiService {
+  private url = '/api/v1/scpi';
+  public _scpis: ReplaySubject<Scpis> = new ReplaySubject<Scpis>(1);
+  constructor(private http: HttpClient) {}
+
+  get scpi$(): Observable<Scpis> {
+    return this._scpis.asObservable();
+  }
+
+  get(): Observable<Scpis> {
+    return this.http.get<Scpis>(this.url).pipe(
+      tap((scpis: Scpis) => {
+        this._scpis.next(scpis);
+      })
+    );
+  }
+}
