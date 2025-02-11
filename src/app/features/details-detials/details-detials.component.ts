2901415b3351c5e1848210ea3d41671ba95a5ade
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-11 14:22:07 +0100

    US-Details-Details-front - start web page creation

--- src/app/features/details-detials/details-detials.component.ts
+++ src/app/features/details-detials/details-detials.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from "../../core/template/components/sidebar/sidebar.component";
+import { NavbarComponent } from "../../core/template/components/navbar/navbar.component";
+import { FooterComponent } from "../../core/template/components/footer/footer.component";
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-details-detials',
-  imports: [],
+  imports: [ IftaLabelModule, FormsModule, CommonModule,ButtonModule, CardModule, DividerModule,PanelModule],
   templateUrl: './details-detials.component.html',
   styleUrl: './details-detials.component.css'
 })
-export class DetailsDetialsComponent {
+export class DetailsDetialsComponent implements OnInit{
 
+  ngOnInit(): void {
+      
+  }
+
+  getTheDetails(){
+    
+  }
 }
