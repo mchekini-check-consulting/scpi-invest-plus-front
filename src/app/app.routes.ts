fe1400a3406b34cc41bfb79099b5f5405361f67e
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-11 11:27:17 +0100

    US details-details-front - component creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from './core/template/container/template.component';
 import {ScpiComponent} from './features/scpi/scpi.component';
+import { DetailsDetialsComponent } from './features/details-detials/details-detials.component';
 
 export const routes: Routes = [
 
@@ -8,9 +9,15 @@ export const routes: Routes = [
     path: '', component: TemplateComponent, children: [
       {
         path: 'scpi', component: ScpiComponent
+      },
+      {
+        path:"details",
+        component: DetailsDetialsComponent
       }
+    
     ],
-  }
-
+    
+  },
+ 
 
 ];
---
+++ src/app/features/details-detials/details-detials.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-detials',
+  imports: [],
+  templateUrl: './details-detials.component.html',
+  styleUrl: './details-detials.component.css'
+})
+export class DetailsDetialsComponent {
+
+}
