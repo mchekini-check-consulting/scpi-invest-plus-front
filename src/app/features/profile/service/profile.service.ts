486867aa1b810932517a5d108eafb11dd08ce58b
kouci <mansourakociela@gmail.com>
2025-02-12 18:09:58 +0100

    localohost removed -profileService

--- src/app/features/profile/service/profile.service.ts
+++ src/app/features/profile/service/profile.service.ts
@@ -3,10 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
-  private apiUrl = `http://localhost:8081/api/investors`;
+  private apiUrl = `/api/investors`;
 
   constructor(private http: HttpClient) {}
 
