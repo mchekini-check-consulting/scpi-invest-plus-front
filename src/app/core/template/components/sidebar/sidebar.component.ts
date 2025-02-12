eb5b51d3f612e7539c86d8ebc7b2c0a06f855be7
mahdi chekini <mchekini@checkconsulting.fr>
2025-02-12 14:54:21 +0100

    Update footer

--- src/app/core/template/components/sidebar/sidebar.component.ts
+++ src/app/core/template/components/sidebar/sidebar.component.ts
@@ -31,7 +31,7 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
 
   menuItems: RouteInfo[] = [];
-  version = "1.0.0";
+  version = "2.0.0";
 
   constructor() {
   }
