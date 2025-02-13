484d516c6a73eee346d4500c4364db01cf0d3046
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-13 02:37:20 +0100

    Created the 4 boxes for global scpi view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import {Routes} from '@angular/router';
 import {TemplateComponent} from './core/template/container/template.component';
 import {ScpiComponent} from './features/scpi/scpi.component';
 import { DetailsDetialsComponent } from './features/details-detials/details-detials.component';
+import { DetailsComponent } from './features/details/details.component';
+import { DetailsGlobalViewComponent } from './features/details-global-view/details-global-view.component';
 import {AuthGuard} from './core/guard/auth.guard';
 
 export const routes: Routes = [
@@ -11,6 +13,25 @@ export const routes: Routes = [
       {
         path: 'scpi', component: ScpiComponent
       },
+      {
+        path:"index",
+        component: DetailsComponent,
+        children:[
+          {
+            path: "details",
+          component: DetailsDetialsComponent,
+         },
+         {
+          path: "vue",
+          component:  DetailsGlobalViewComponent
+         }
+        ]
+      }
+
+    ],
+
+  },
+
 
 
     ],
