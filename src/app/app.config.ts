d6a31be616350877f164cb729adea9f5e4fe796b
Ouali Abdiche <oualiabdiche@outlook.fr>
2025-02-13 18:48:20 +0100

    Implementation of Authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,15 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import Aura from '@primeng/themes/aura';
-import { providePrimeNG } from 'primeng/config';
-import { routes } from './app.routes';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {OAuthModule, OAuthService} from 'angular-oauth2-oidc';
+import {HttpRequestInterceptor} from './core/interceptor/HttpInterceptor';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, 'i18n/', '.json');
@@ -24,23 +21,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     importProvidersFrom(BrowserAnimationsModule),
     provideAnimationsAsync(),
-    provideHttpClient(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: '.my-app-dark',
-        },
-      },
-    }),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: createTranslateLoader,
-          deps: [HttpClient],
-        },
-      })
-    ),
-  ],
+    provideHttpClient(withInterceptors([HttpRequestInterceptor])),
+    providePrimeNG({theme: {preset: Aura}}),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })),
+    { provide: OAuthService, useClass: OAuthService },
+    importProvidersFrom(OAuthModule.forRoot()),
+  ]
 };
---
+++ src/app/core/service/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {OAuthService} from 'angular-oauth2-oidc';
+import {authCodeFlowConfig} from '../config/auth.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private isLoggedOut = false;
+
+  constructor(private oauthService: OAuthService) {
+    this.configureOAuth();
+  }
+
+  private configureOAuth() {
+    this.oauthService.configure(authCodeFlowConfig);
+
+    this.oauthService.loadDiscoveryDocumentAndTryLogin().then(() => {
+      if (!this.isLoggedOut && !this.oauthService.hasValidAccessToken()) {
+        this.oauthService.initLoginFlow();
+      }
+    });
+  }
+
+  logout() {
+    this.isLoggedOut = true;
+    this.oauthService.logOut();
+    this.oauthService.logOut(true);
+    // @ts-ignore
+    this.oauthService.clearHashAfterLogin();
+  }
+
+  isLoggedIn(): boolean {
+    return this.oauthService.hasValidAccessToken();
+  }
+}
