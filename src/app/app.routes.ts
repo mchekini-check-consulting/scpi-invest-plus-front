ba9409fff6951e54c3de3a7f098ea52cf5021bd9
Ouali Abdiche <oualiabdiche@outlook.fr>
2025-02-13 18:48:20 +0100

    Implementation of Authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from './core/template/container/template.component';
 import {ScpiComponent} from './features/scpi/scpi.component';
+import {AuthGuard} from './core/guard/auth.guard';
 
 export const routes: Routes = [
 
   {
-    path: '', component: TemplateComponent, children: [
+    path: '', component: TemplateComponent,
+    children: [
       {
-        path: 'scpi', component: ScpiComponent
+        path: 'scpi', component: ScpiComponent, canActivate: [AuthGuard],
       }
     ],
   }
-
-
 ];
