94cb3689f5ef9e23c57c10e53b9a88daf30d0a43
mahdi chekini <mchekini@checkconsulting.fr>
2025-02-14 21:29:46 +0100

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from './core/template/container/template.component';
 import {ScpiComponent} from './features/scpi/scpi.component';
+import { ProfileComponent } from './features/profile/components/profile.component';
 
 export const routes: Routes = [
 
@@ -8,6 +9,9 @@ export const routes: Routes = [
     path: '', component: TemplateComponent, children: [
       {
         path: 'scpi', component: ScpiComponent
+      },
+      {
+        path: 'profile', component: ProfileComponent
       }
     ],
   }
---
+++ src/app/features/profile/service/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  private apiUrl = `/api/v1/investors`;
+
+  constructor(private http: HttpClient) {}
+
+  getInvestorByEmail(email: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${email}`);
+  }
+
+  updateInvestor(email: string, investorData: any): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${email}`, investorData);
+  }
+}
