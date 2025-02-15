0d5c53ed7ee1f78f8695e98f2663b7402fb6071b
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-15 19:53:52 +0100

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from './core/template/container/template.component';
 import {ScpiComponent} from './features/scpi/scpi.component';
-import { DetailsDetialsComponent } from './features/details-detials/details-detials.component';
-import { DetailsComponent } from './features/details/details.component';
-import { DetailsGlobalViewComponent } from './features/details-global-view/details-global-view.component';
+import { DetailsDetialsComponent } from './features/scpi/details-detials/details-detials.component';
+import { DetailsComponent } from './features/scpi/details/details.component';
+import { DetailsGlobalViewComponent } from './features/scpi/details-global-view/details-global-view.component';
 import {AuthGuard} from './core/guard/auth.guard';
 
 export const routes: Routes = [
