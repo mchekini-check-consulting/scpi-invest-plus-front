b7dcd9b959c582c9e1397d270cbb143c1e9080e6
kouci <mansourakociela@gmail.com>
2025-02-16 19:39:20 +0100

    wip

---
+++ src/app/models/Sector.ts
@@ -0,0 +1,17 @@
+export class SectorId{
+    scpiId: number;
+    name: string;
+    constructor(scpiId:number , name:string){
+        this.scpiId = scpiId;
+        this.name = name;
+    }
+}
+export class Sector {
+    id:SectorId;
+    sectorPercentage: number;
+
+    constructor(id:SectorId, sectorPercentage: number) {
+        this.id = id;
+        this.sectorPercentage = sectorPercentage;
+    }
+}
