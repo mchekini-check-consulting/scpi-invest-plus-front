95e2e86824f278f5a126bfea22881d1bb450022b
TegHocine <61586802+TegHocine@users.noreply.github.com>
2025-02-19 18:24:28 +0100

    Fix dark mode auto selecting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,23 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {routes} from './app.routes';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {providePrimeNG} from 'primeng/config';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import Aura from '@primeng/themes/aura';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {OAuthModule, OAuthService} from 'angular-oauth2-oidc';
-import {HttpRequestInterceptor} from './core/interceptor/HttpInterceptor';
+import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+import { HttpRequestInterceptor } from './core/interceptor/HttpInterceptor';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, 'i18n/', '.json');
@@ -22,15 +30,24 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(BrowserAnimationsModule),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([HttpRequestInterceptor])),
-    providePrimeNG({theme: {preset: Aura}}),
-    importProvidersFrom(TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient]
-      }
-    })),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.my-app-dark',
+        },
+      },
+    }),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      })
+    ),
     { provide: OAuthService, useClass: OAuthService },
     importProvidersFrom(OAuthModule.forRoot()),
-  ]
+  ],
 };
