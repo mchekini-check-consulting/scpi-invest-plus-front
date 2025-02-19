1582b4c0b7d4d234ea32c37ecaa4832866d8a642
FaresBssm <faresboussoum2022@gmail.com>
2025-02-19 15:29:53 +0100

    wip

--- src/app/core/service/scpi.service.ts
+++ src/app/core/service/scpi.service.ts
@@ -1,7 +1,7 @@
-import {HttpClient, HttpParams} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ScpiModel } from '../model/scpi.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {ScpiModel} from '../model/scpi.model';
 import {ScpiSearch} from '@/core/model/scpi-search.model';
 
 type Scpis = ScpiModel[];
@@ -14,11 +14,9 @@ export class ScpiService {
   private url = '/api/v1/scpi';
 
   constructor(private http: HttpClient) {}
-
   get(): Observable<Scpis> {
     return this.http.get<Scpis>(this.url);
   }
-
   getScpiWithFilter(search?:ScpiSearch): Observable<ScpiModel[]> {
     return this.http.post<ScpiModel[]>(`${this.url}/search`,search);
   }
--- src/app/features/search-multicriteria/search-multicriteria.component.ts
+++ src/app/features/search-multicriteria/search-multicriteria.component.ts
@@ -9,6 +9,7 @@ import { ButtonDirective } from 'primeng/button';
 import { ScpiSearch } from "@/core/model/scpi-search.model";
 import { ScpiService } from "@/core/service/scpi.service";
 import { ScpiModel } from '@/core/model/scpi.model';
+import {SearchBarComponent} from "@/features/search-multicriteria/components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-search-multicriteria',
@@ -20,7 +21,8 @@ import { ScpiModel } from '@/core/model/scpi.model';
     Slider,
     MultiSelect,
     SelectButton,
-    ButtonDirective
+    ButtonDirective,
+    SearchBarComponent
   ],
   templateUrl: './search-multicriteria.component.html',
   styleUrls: ['./search-multicriteria.component.css']
@@ -41,6 +43,101 @@ export class SearchMulticriteriaComponent {
     rentalFrequency: undefined,
     minInvestment: 0
   };
+  constructor(private scpiService: ScpiService) {}
+  applyFilters() {
+    this.isFilterVisible = false;
+    this.searchScpi();
+  }
+
+  onSearchTermChanged(searchTerm: string) {
+    this.filters.searchTerm = searchTerm;
+    this.searchScpi();
+  }
+
+  searchScpi() {
+    if (this.isSearchDisabled()) {
+      return;
+    }
+    let filtersToSend: ScpiSearch = this.prepareFilters();
+    this.loading = true;
+    this.scpiService.getScpiWithFilter(filtersToSend).subscribe(
+      (data) => {
+        this.scpiResults = data || [];
+        this.loading = false;
+        this.scpiFiltered.emit(this.scpiResults);
+      },
+      (error) => {
+        this.scpiResults = [];
+        this.loading = false;
+        this.scpiFiltered.emit(this.scpiResults);
+      }
+    );
+  }
+
+  isSearchDisabled(): boolean {
+    return !Object.values(this.filters).some(value =>
+      (Array.isArray(value) && value.length > 0) ||
+      (typeof value === 'number' && value > 0) ||
+      (typeof value === 'string' && value.trim() !== "")
+    );
+  }
+
+  resetFilters() {
+    this.filters = this.getDefaultFilters();
+    this.isFilterVisible = false;
+    this.scpiService.get().subscribe(
+      (data) => {
+        this.scpiResults = data || [];
+        this.loading = false;
+        this.scpiFiltered.emit(this.scpiResults);
+      },
+      (error) => {
+        this.scpiResults = [];
+        this.loading = false;
+        this.scpiFiltered.emit(this.scpiResults);
+      }
+    );
+  }
+  private prepareFilters(): ScpiSearch {
+    let filtersToSend: ScpiSearch = {
+      ...this.filters,
+      locations: this.filters.locations && Array.isArray(this.filters.locations)
+        ? this.filters.locations.map((loc: any) => loc.value ?? loc)
+        : [],
+      sectors: this.filters.sectors && Array.isArray(this.filters.sectors)
+        ? this.filters.sectors.map((sec: any) => sec.value ?? sec)
+        : [],
+      distributionRate: this.filters.distributionRate !== undefined && this.filters.distributionRate !== 0
+        ? this.filters.distributionRate
+        : undefined,
+      minInvestment: this.filters.minInvestment !== undefined && this.filters.minInvestment !== null
+        ? this.filters.minInvestment
+        : 0
+    };
+
+    filtersToSend = Object.fromEntries(
+      Object.entries(filtersToSend).filter(([_, v]) =>
+        v !== undefined && v !== null && v !== "" && (!Array.isArray(v) || v.length > 0)
+      )
+    ) as ScpiSearch;
+
+    return filtersToSend;
+  }
+  private getDefaultFilters(): ScpiSearch {
+    return {
+      searchTerm: undefined,
+      locations: undefined,
+      sectors: undefined,
+      minimumSubscription: 0,
+      subscriptionFees: undefined,
+      distributionRate: undefined,
+      rentalFrequency: undefined,
+      minInvestment: 0
+    };
+  }
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
   investmentZones = [
     { label: 'Europe', value: 'Europe' },
     { label: 'Grande-Bretagne', value: 'Grande-Bretagne' },
@@ -66,96 +163,12 @@ export class SearchMulticriteriaComponent {
     {label: 'Transport', value: 'Transport'},
 
   ];
-
   subscriptionFeesOptions = [
     { label: 'Avec frais', value: true },
     { label: 'Sans frais', value: false }
   ];
-
   rentalFrequencyOptions = [
     { label: 'Mensuelle', value: 'Mensuelle' },
     { label: 'Trimestrielle', value: 'Trimestrielle' }
   ];
-
-  constructor(private scpiService: ScpiService) {}
-
-  applyFilters() {
-    this.isFilterVisible = false;
-    this.searchScpi();
-  }
-  searchScpi() {
-    if (this.isSearchDisabled()) {
-      return;
-    }
-
-    let filtersToSend: ScpiSearch = {
-      ...this.filters,
-      locations: this.filters.locations && Array.isArray(this.filters.locations)
-        ? this.filters.locations.map((loc: any) => loc.value ?? loc)
-        : [],
-      sectors: this.filters.sectors && Array.isArray(this.filters.sectors)
-        ? this.filters.sectors.map((sec: any) => sec.value ?? sec)
-        : [],
-      distributionRate: this.filters.distributionRate !== undefined && this.filters.distributionRate !== 0
-        ? this.filters.distributionRate
-        : undefined,
-      minInvestment: this.filters.minInvestment !== undefined && this.filters.minInvestment !== null
-        ? this.filters.minInvestment
-        : 0
-    };
-    filtersToSend = Object.fromEntries(
-      Object.entries(filtersToSend).filter(([_, v]) =>
-        v !== undefined && v !== null && v !== "" && (!Array.isArray(v) || v.length > 0)
-      )
-    ) as ScpiSearch;
-
-    this.loading = true;
-    console.log("🔍 Recherche SCPI avec filtres :", this.filters);
-
-    this.scpiService.getScpiWithFilter(filtersToSend).subscribe(
-      (data) => {
-        console.log("SCPI reçues :", data);
-        this.scpiResults = data || [];
-        this.loading = false;
-        this.scpiFiltered.emit(this.scpiResults);
-      },
-      (error) => {
-        console.error('Erreur lors de la récupération des SCPI', error);
-        this.scpiResults = [];
-        this.loading = false;
-        this.scpiFiltered.emit(this.scpiResults);
-      }
-    );
-  }
-
-  isSearchDisabled(): boolean {
-    return !(
-      (this.filters.distributionRate ?? 0) > 0 ||
-      (this.filters.locations?.length ?? 0) > 0 ||
-      (this.filters.sectors?.length ?? 0) > 0 ||
-      this.filters.subscriptionFees !== undefined ||
-      (this.filters.rentalFrequency && this.filters.rentalFrequency.trim() !== '') ||
-      (this.filters.minInvestment !== undefined && this.filters.minInvestment >= 0)
-    );
-  }
-  stopPropagation(event: Event) {
-    event.stopPropagation();
-  }
-  resetFilters() {
-    this.filters = {
-      searchTerm: undefined,
-      locations:undefined,
-      sectors: undefined,
-      minimumSubscription: 0,
-      subscriptionFees: undefined,
-      distributionRate: undefined,
-      rentalFrequency: undefined,
-      minInvestment: 0
-    };
-    this.scpiService.get().subscribe((data) => {
-      this.scpiResults = data;
-      this.scpiFiltered.emit(this.scpiResults);
-    });
-  }
-
 }
