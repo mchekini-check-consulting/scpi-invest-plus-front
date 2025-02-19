91029bab19f842d8822839389f991b2a66937132
FaresBssm <159578369+FaresBssm@users.noreply.github.com>
2025-02-19 15:46:18 +0100

    Us 14 recherche multi critère & search bar(#15)

---
+++ src/app/core/model/scpi-search.model.ts
@@ -0,0 +1,10 @@
+export interface ScpiSearch {
+  searchTerm?: string;
+  locations?: string[];
+  sectors?: string[];
+  minimumSubscription?: number;
+  subscriptionFees?: boolean;
+  rentalFrequency?: string;
+  distributionRate?: number;
+  minInvestment?: number;
+}
