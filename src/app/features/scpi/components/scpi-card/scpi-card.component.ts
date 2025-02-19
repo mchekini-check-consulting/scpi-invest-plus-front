53e4e5f2930a8a40b815ec81e75c4bc0753f4cc5
T.Hocine <61586802+TegHocine@users.noreply.github.com>
2025-02-19 18:28:10 +0100

    Fix/dark mode (#21)
    Fix dark mode auto selecting

--- src/app/features/scpi/components/scpi-card/scpi-card.component.ts
+++ src/app/features/scpi/components/scpi-card/scpi-card.component.ts
@@ -43,21 +43,21 @@ export class ScpiCardComponent {
     const percentage =
       sector.sectorPercentage !== undefined
         ? `${sector.sectorPercentage}%`
-        : 'N/A%';
+        : '';
     return `${name} - ${percentage}`;
   }
 
   formatDistributionRate() {
     const distributionRate = this.scpi?.statYear?.distributionRate;
     return `Rendement - ${
-      distributionRate !== undefined ? distributionRate + '%' : 'N/A%'
+      distributionRate !== undefined ? distributionRate + '%' : 'N/A'
     }`;
   }
 
   formatMinimum() {
     const minimumSubscription = this.scpi?.minimumSubscription;
     return `Minimum - ${
-      minimumSubscription !== undefined ? minimumSubscription + ' EUR' : 'N/A'
+      minimumSubscription !== undefined ? minimumSubscription + ' €' : 'N/A'
     }`;
   }
 }
