2c7c226e804615e07be0e5aa4cded1fd3308b942
speedkillsx <amayaslabchri88@gmail.com>
2025-03-07 15:24:18 +0100

    Update names folders for details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
-import {Routes} from '@angular/router';
-import {TemplateComponent} from './core/template/container/template.component';
-import {ScpiComponent} from './features/scpi/scpi.component';
-import {DetailsDetialsComponent} from '@/features/scpi/details/components/details-detials/details-detials.component';
-import {DetailsComponent} from './features/scpi/details/container/details.component';
-import {DetailsGlobalViewComponent} from '@/features/scpi/details/components/details-global-view/details-global-view.component';
-import {PortefeuilleComponent} from './features/portefeuille/components/portefeuille.component'
-import {AuthGuard} from './core/guard/auth.guard';
-import {ProfileComponent} from './features/profile/components/profile.component';
-import {ScpiHistoryDetailsComponent} from './features/scpi/details/components/details-history/scpi-history-details.component';
-import {SimulationComponent} from './features/simulation/container/simulation.component';
-import {ComparatorComponent} from '@/features/comparator/comparator.component';
-import {SimulationDetailComponent} from '@/features/simulation/simulation-detail/simulation-detail.component';
+import { Routes } from '@angular/router';
+import { TemplateComponent } from './core/template/container/template.component';
+import { ScpiComponent } from './features/scpi/scpi.component';
+import { DetailsDetialsComponent } from '@/features/scpi/details/components/scpi-details/details-detials.component';
+import { DetailsComponent } from './features/scpi/details/container/details.component';
+import { DetailsGlobalViewComponent } from '@/features/scpi/details/components/scpi-global-view/details-global-view.component';
+import { PortefeuilleComponent } from './features/portefeuille/components/portefeuille.component';
+import { AuthGuard } from './core/guard/auth.guard';
+import { ProfileComponent } from './features/profile/components/profile.component';
+import { ScpiHistoryDetailsComponent } from './features/scpi/details/components/scpi-history/scpi-history-details.component';
+import { SimulationComponent } from './features/simulation/container/simulation.component';
+import { ComparatorComponent } from '@/features/comparator/comparator.component';
+import { SimulationDetailComponent } from '@/features/simulation/simulation-detail/simulation-detail.component';
 
 export const routes: Routes = [
   {
@@ -38,8 +38,8 @@ export const routes: Routes = [
           {
             path: 'details/:id',
             component: SimulationDetailComponent,
-          }
-        ]
+          },
+        ],
       },
       {
         path: 'comparateur',
@@ -66,8 +66,9 @@ export const routes: Routes = [
         ],
       },
       {
-        path: 'portefeuille', component: PortefeuilleComponent,
-      }
+        path: 'portefeuille',
+        component: PortefeuilleComponent,
+      },
     ],
   },
 ];
