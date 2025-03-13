45bc15324b287768f101e4ad3a7d1ee932d5aef4
kouci <mansourakociela@gmail.com>
2025-03-13 18:36:19 +0100

    wip

--- src/app/features/simulation/simulation-detail/simulation-detail.component.ts
+++ src/app/features/simulation/simulation-detail/simulation-detail.component.ts
@@ -1,21 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {
-  AddScpiToSimulationComponent
-} from '@/features/simulation/components/add-scpi-to-simulation/add-scpi-to-simulation.component';
-import {Card} from 'primeng/card';
-import {PrimeTemplate} from 'primeng/api';
-import {ActivatedRoute} from '@angular/router';
-import {SimulationService} from '@/core/service/simulation.service';
-import {Simulation, ScpiSimulation} from '@/core/model/Simulation';
-import {Panel} from 'primeng/panel';
-import {DecimalPipe, NgForOf} from '@angular/common';
-import {MapComponent} from '@/shared/component/map/map.component';
-import {Location} from '@/core/model/Location';
-import {ChartModule} from 'primeng/chart';
-import {Sector} from '@/core/model/Sector';
+import { Component, Input, OnInit } from "@angular/core";
+import { AddScpiToSimulationComponent } from "@/features/simulation/components/add-scpi-to-simulation/add-scpi-to-simulation.component";
+import { Card } from "primeng/card";
+import { PrimeTemplate } from "primeng/api";
+import { ActivatedRoute } from "@angular/router";
+import { SimulationService } from "@/core/service/simulation.service";
+import { Simulation, ScpiSimulation } from "@/core/model/Simulation";
+import { Panel } from "primeng/panel";
+import { DecimalPipe } from "@angular/common";
+import { MapComponent } from "@/shared/component/map/map.component";
+import { Location } from "@/core/model/Location";
+import { ChartModule } from "primeng/chart";
+import { Sector } from "@/core/model/Sector";
 
 @Component({
-  selector: 'app-simulation-detail',
+  selector: "app-simulation-detail",
   imports: [
     AddScpiToSimulationComponent,
     Card,
@@ -24,16 +22,12 @@ import {Sector} from '@/core/model/Sector';
     DecimalPipe,
     MapComponent,
     ChartModule,
-    NgForOf,
   ],
-  templateUrl: './simulation-detail.component.html',
-  styleUrls: ['./simulation-detail.component.css'],
-  standalone: true
+  templateUrl: "./simulation-detail.component.html",
+  styleUrls: ["./simulation-detail.component.css"],
 })
 export class SimulationDetailComponent implements OnInit {
-  totalGrossRevenue: number = 0;
-  totalNetRevenue: number = 0;
-  simulationId: string = '';
+  simulationId: string = "";
   simulation: Simulation | null = null;
   ListeLocations: Location[] = [];
 
@@ -42,7 +36,7 @@ export class SimulationDetailComponent implements OnInit {
     plugins: {
       legend: {
         display: true,
-        position: 'top',
+        position: "top",
       },
     },
   };
@@ -59,12 +53,11 @@ export class SimulationDetailComponent implements OnInit {
     datasets: [
       {
         data: [],
-        backgroundColor: ['#5A54F9', '#8674FC', '#C084FC'],
-        hoverBackgroundColor: ['#7165FA', '#9D8AFC', '#D29FFC'],
+        backgroundColor: ["#5A54F9", "#8674FC", "#C084FC"],
+        hoverBackgroundColor: ["#7165FA", "#9D8AFC", "#D29FFC"],
       },
     ],
   };
-
 
   geographicalData: {
     labels: string[];
@@ -78,39 +71,36 @@ export class SimulationDetailComponent implements OnInit {
     datasets: [
       {
         data: [],
-        backgroundColor: ['#5A54F9', '#8674FC', '#C084FC'],
-        hoverBackgroundColor: ['#7165FA', '#9D8AFC', '#D29FFC'],
+        backgroundColor: ["#5A54F9", "#8674FC", "#C084FC"],
+        hoverBackgroundColor: ["#7165FA", "#9D8AFC", "#D29FFC"],
       },
     ],
   };
-
 
   constructor(
     private route: ActivatedRoute,
     private simulationService: SimulationService
   ) {
-    const id = this.route.snapshot.paramMap.get('id');
+    const id = this.route.snapshot.paramMap.get("id");
     if (id) {
       this.simulationId = id;
-      this.simulationService.getSimulationById(id).subscribe()
-      this.simulationService.simulation$.subscribe(simulation => {
+      this.simulationService.getSimulationById(id).subscribe();
+      this.simulationService.simulation$.subscribe((simulation) => {
         this.simulation = simulation;
         this.updateSectorData();
         this.updateLocationData();
         this.updateScpiInvestmentData();
-        this.updateRevenues();
       });
     }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   updateSectorData(): void {
     if (this.simulation?.sectors && this.simulation.sectors.length > 0) {
       this.sectorData.labels = this.simulation.sectors.map(
         (sector: Sector) =>
-          sector.id.name + ' (' + sector.sectorPercentage + '%)'
+          sector.id.name + " (" + sector.sectorPercentage + "%)"
       );
       this.sectorData.datasets[0].data = this.simulation.sectors.map(
         (sector: Sector) => sector.sectorPercentage
@@ -119,11 +109,16 @@ export class SimulationDetailComponent implements OnInit {
   }
 
   updateScpiInvestmentData(): void {
-    if (this.simulation?.scpiSimulations && this.simulation.scpiSimulations.length > 0) {
+    if (
+      this.simulation?.scpiSimulations &&
+      this.simulation.scpiSimulations.length > 0
+    ) {
       const totalInvestment = this.simulation.totalInvestment;
 
       if (totalInvestment === 0) {
-        console.warn("⚠️ Le total de l'investissement est à 0, impossible de calculer les pourcentages.");
+        console.warn(
+          "⚠️ Le total de l'investissement est à 0, impossible de calculer les pourcentages."
+        );
         return;
       }
 
@@ -131,14 +126,12 @@ export class SimulationDetailComponent implements OnInit {
         (scpi: ScpiSimulation) => scpi.scpiName || `SCPI ${scpi.scpiId}`
       );
 
-      this.geographicalData.datasets[0].data = this.simulation.scpiSimulations.map(
-        (scpi: ScpiSimulation) => parseFloat(((scpi.rising / totalInvestment) * 100).toFixed(2))
-      );
-
-
+      this.geographicalData.datasets[0].data =
+        this.simulation.scpiSimulations.map((scpi: ScpiSimulation) =>
+          parseFloat(((scpi.rising / totalInvestment) * 100).toFixed(2))
+        );
     }
   }
-
 
   updateLocationData(): void {
     if (
@@ -154,20 +147,4 @@ export class SimulationDetailComponent implements OnInit {
       }));
     }
   }
-
-  updateRevenues(): void {
-    if (this.simulation?.scpiSimulations && this.simulation.scpiSimulations.length > 0) {
-      this.totalGrossRevenue = this.simulation.scpiSimulations.reduce(
-        (acc, scpi) => acc + (scpi.grossRevenue??0), 0
-      );
-      this.totalNetRevenue = this.simulation.scpiSimulations.reduce(
-        (acc, scpi) => acc + (scpi.netRevenue??0), 0
-      );
-    }
-  }
-
-  trackByScpiId(index: number, scpi: ScpiSimulation): number {
-    return scpi.scpiId;
-  }
-
 }
