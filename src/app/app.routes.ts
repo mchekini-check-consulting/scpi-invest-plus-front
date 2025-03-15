72ab832687ba5bf19dd4ef69a83778c479b974ec
TegHocine <61586802+TegHocine@users.noreply.github.com>
2025-03-15 13:20:29 +0100

    Set SCPI page as default instead of an empty page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,30 @@
-import { Routes } from '@angular/router';
-import { TemplateComponent } from './core/template/container/template.component';
-import { ScpiComponent } from './features/scpi/scpi.component';
-import { DetailsDetialsComponent } from '@/features/scpi/details/components/scpi-details/details-detials.component';
-import { DetailsComponent } from './features/scpi/details/container/details.component';
-import { DetailsGlobalViewComponent } from '@/features/scpi/details/components/scpi-global-view/details-global-view.component';
-import { PortefeuilleComponent } from './features/portefeuille/components/portefeuille.component';
-import { AuthGuard } from './core/guard/auth.guard';
-import { ProfileComponent } from './features/profile/components/profile.component';
-import { ScpiHistoryDetailsComponent } from './features/scpi/details/components/scpi-history/scpi-history-details.component';
-import { SimulationComponent } from './features/simulation/container/simulation.component';
-import { ComparatorComponent } from '@/features/comparator/comparator.component';
-import { SimulationDetailComponent } from '@/features/simulation/simulation-detail/simulation-detail.component';
+import {Routes} from '@angular/router';
+import {TemplateComponent} from './core/template/container/template.component';
+import {ScpiComponent} from './features/scpi/scpi.component';
+import {DetailsDetialsComponent} from '@/features/scpi/details/components/scpi-details/details-detials.component';
+import {DetailsComponent} from './features/scpi/details/container/details.component';
+import {
+  DetailsGlobalViewComponent
+} from '@/features/scpi/details/components/scpi-global-view/details-global-view.component';
+import {PortefeuilleComponent} from './features/portefeuille/components/portefeuille.component';
+import {AuthGuard} from './core/guard/auth.guard';
+import {ProfileComponent} from './features/profile/components/profile.component';
+import {
+  ScpiHistoryDetailsComponent
+} from './features/scpi/details/components/scpi-history/scpi-history-details.component';
+import {SimulationComponent} from './features/simulation/container/simulation.component';
+import {ComparatorComponent} from '@/features/comparator/comparator.component';
+import {SimulationDetailComponent} from '@/features/simulation/simulation-detail/simulation-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: TemplateComponent,
     children: [
+      {
+        path: '',
+        component: ScpiComponent,
+      },
       {
         path: 'scpi',
         component: ScpiComponent,
--- src/app/features/scpi/scpi.component.ts
+++ src/app/features/scpi/scpi.component.ts
@@ -1,21 +1,16 @@
-import { ScpiModel } from '@/core/model/scpi.model';
-import { ScpiService } from '@/core/service/scpi.service';
-import { SearchMulticriteriaComponent } from '@/features/search-multicriteria/search-multicriteria.component';
-import { CommonModule } from '@angular/common';
-import { SkeletonModule } from 'primeng/skeleton';
-import {
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
-import { catchError, Subscription } from 'rxjs';
-import { ScpiCardComponent } from './components/scpi-card/scpi-card.component';
+import {ScpiModel} from '@/core/model/scpi.model';
+import {ScpiService} from '@/core/service/scpi.service';
+import {SearchMulticriteriaComponent} from '@/features/search-multicriteria/search-multicriteria.component';
+import {CommonModule} from '@angular/common';
+import {SkeletonModule} from 'primeng/skeleton';
+import {ChangeDetectorRef, Component, Input, OnDestroy, OnInit,} from '@angular/core';
+import {catchError, Subscription} from 'rxjs';
+import {ScpiCardComponent} from './components/scpi-card/scpi-card.component';
+import {UserService} from '@/core/service/user.service';
 
 @Component({
   selector: 'app-scpi',
-  imports: [ScpiCardComponent, SearchMulticriteriaComponent, CommonModule,SkeletonModule],
+  imports: [ScpiCardComponent, SearchMulticriteriaComponent, CommonModule, SkeletonModule],
   templateUrl: './scpi.component.html',
   styleUrl: './scpi.component.css',
 })
@@ -27,42 +22,50 @@ export class ScpiComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
 
 
-
   @Input() isAddingScpi = false;
-  @Input() addScpi? : boolean;
+  @Input() addScpi?: boolean;
   skeletonArray = new Array(10);
-  images = Array.from({ length: 10 }, (_, i) => `img/scpi/${i + 1}.webp`);
+  images = Array.from({length: 10}, (_, i) => `img/scpi/${i + 1}.webp`);
 
   constructor(
     private scpiService: ScpiService,
-    private cdRef: ChangeDetectorRef
-  ) {}
+    private cdRef: ChangeDetectorRef,
+    private userService: UserService) {
+  }
 
   ngOnInit() {
-    if (this.filteredScpis.length === 0) {
-      this.loading = true;
-      this.subscriptions.add(
-        this.scpiService
-          .get()
-          .pipe(
-            catchError((error) => {
+
+    this.userService.user$.subscribe(user => {
+      if (!user) {
+        return
+      }
+
+      if (this.filteredScpis.length === 0) {
+        this.loading = true;
+        this.subscriptions.add(
+          this.scpiService
+            .get()
+            .pipe(
+              catchError((error) => {
+                this.loading = false;
+                return [];
+              })
+            )
+            .subscribe((data) => {
+              this.scpis = data;
+              this.filteredScpis = [...data];
               this.loading = false;
-              return [];
+              this.cdRef.detectChanges();
             })
-          )
-          .subscribe((data) => {
-            this.scpis = data;
-            this.filteredScpis = [...data];
-            this.loading = false;
-            this.cdRef.detectChanges();
-          })
-      );
-    }
+        )
+      }
+    })
   }
 
   getImage(id: number): string {
     return this.images[id % this.images.length];
   }
+
   updateScpiList(filteredList: ScpiModel[] | null | undefined) {
     if (filteredList && filteredList.length > 0) {
       this.filteredScpis = [...filteredList];
@@ -71,6 +74,7 @@ export class ScpiComponent implements OnInit, OnDestroy {
     }
     this.cdRef.detectChanges();
   }
+
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
   }
