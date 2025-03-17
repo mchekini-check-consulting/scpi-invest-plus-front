6b3c52c76799ca0a0f72dee0ef188636a0a4c3f2
Litissia Hadjout <litissia.hadjout@gmail.com>
2025-03-17 16:29:54 +0100

    improve_ui_portfolio

--- src/app/features/portefeuille/components/portefeuille.component.ts
+++ src/app/features/portefeuille/components/portefeuille.component.ts
@@ -1,45 +1,41 @@
-import { Investments } from '@/core/model/Investments';
-import { InvestmentService } from '@/core/service/InvestmentService';
-import { CommonModule } from '@angular/common';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { Investments } from "@/core/model/Investments";
+import { InvestmentService } from "@/core/service/InvestmentService";
+import { CommonModule } from "@angular/common";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ButtonModule } from "primeng/button";
 
 @Component({
-  selector: 'app-portefeuille',
+  selector: "app-portefeuille",
   imports: [ButtonModule, CommonModule],
-  templateUrl: './portefeuille.component.html',
-  styleUrl: './portefeuille.component.css'
+  templateUrl: "./portefeuille.component.html",
+  styleUrl: "./portefeuille.component.css",
 })
-
-export class PortefeuilleComponent implements OnInit{
-
-  investissements: Investments[] = []
+export class PortefeuilleComponent implements OnInit {
+  investissements: Investments[] = [];
 
   constructor(
-    private investmentService: InvestmentService, 
+    private investmentService: InvestmentService,
     private router: Router
-  ){}
+  ) {}
 
   ngOnInit(): void {
     this.getInvestments();
-
   }
 
-  public getInvestments():void{
+  public getInvestments(): void {
     this.investmentService.getInvestments().subscribe(
-      (response: Investments[])=>{
+      (response: Investments[]) => {
         this.investissements = response;
-        console.log(this.investissements)
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
-    )
+    );
   }
 
-  startInvesting(): void{
-    this.router.navigate(['/scpi'])
+  startInvesting(): void {
+    this.router.navigate(["/scpi"]);
   }
 }
