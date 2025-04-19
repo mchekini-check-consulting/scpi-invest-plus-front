0684114a96707b36b92fde49cddb46f15f75bcfe
TegHocine <61586802+TegHocine@users.noreply.github.com>
2025-04-20 00:38:57 +0100

    Fix auth-guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,6 +15,7 @@ import { CreditComponent } from "@/features/credit/credit.component";
 import { PlanComponent } from "@/features/plan/plan.component";
 import { ScheduledPaymentComponent } from "@/features/scheduled-payment/scheduled-payment.component";
 import { NotFoundComponent } from "@/features/not-found/not-found.component";
+import { UnauthorizedComponent } from "@/features/unauthorized/unauthorized.component";
 
 export const routes: Routes = [
   {
@@ -38,10 +39,12 @@ export const routes: Routes = [
       {
         path: "credit",
         component: CreditComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: "plans",
         component: PlanComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: "simulations",
@@ -88,12 +91,16 @@ export const routes: Routes = [
       {
         path: "portefeuille",
         component: PortefeuilleComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: "scheduled-payment",
         component: ScheduledPaymentComponent,
+        canActivate: [AuthGuard],
       },
+      { path: "unauthorized", component: UnauthorizedComponent },
     ],
   },
+
   { path: "**", component: NotFoundComponent },
 ];
--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,14 +1,58 @@
-import { Injectable } from '@angular/core';
-import { CanActivate,} from '@angular/router';
-import {AuthService} from "../service/auth.service";
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { AuthService } from "../service/auth.service";
+import { UserService } from "@/core/service/user.service";
+import { PlanService } from "@/core/service/plan.service";
+import { PlanModel } from "@/core/model/plan.model";
+import { UserModel } from "@/core/model/user.model";
+import { ROUTES } from "@/core/template/components/sidebar/sidebar.component";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  plans: PlanModel[] = [];
+  user: UserModel | null = null;
 
-  canActivate(): boolean {
-    return this.authService.isLoggedIn();
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private planService: PlanService,
+    private router: Router
+  ) {
+    this.planService.plans$.subscribe((plans) => {
+      this.plans = plans;
+    });
+    this.userService.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  canActivate(_: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!this.authService.isLoggedIn()) return false;
+
+    const requestedPath = state.url;
+    const functionality = ROUTES.find(
+      (route) => route.path === requestedPath
+    )?.feature;
+
+    const hasAccess =
+      (this.user?.role === "Standard" &&
+        this.plans.some(
+          (p) => p.functionality === functionality && p.standard
+        )) ||
+      (this.user?.role === "Premium" &&
+        this.plans.some((p) => p.functionality === functionality && p.premium));
+
+    if (hasAccess) {
+      return true;
+    }
+
+    return this.router.createUrlTree(["/unauthorized"]);
   }
 }
