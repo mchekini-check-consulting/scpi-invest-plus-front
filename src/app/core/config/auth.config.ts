37a7596defa486afd62ba7095ded055c20e0f2b2
Litissia Hadjout <litissia.hadjout@gmail.com>
2025-04-20 15:27:14 +0100

    feature/add_landing_page_redirection_to_keycloack

--- src/app/core/config/auth.config.ts
+++ src/app/core/config/auth.config.ts
@@ -3,11 +3,11 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
   issuer: 'https://keycloak.check-consulting.net/realms/master',
-  redirectUri: window.location.origin,
+  redirectUri: window.location.origin + "/dashboard",
   clientId: 'scpi-invest-plus',
   responseType: 'code',
   logoutUrl : 'https://keycloak.check-consulting.net/realms/master/protocol/openid-connect/logout',
-  postLogoutRedirectUri: window.location.origin,
+  postLogoutRedirectUri: window.location.origin + '',
   showDebugInformation: true,
   scope: 'openid profile email offline_access',
   timeoutFactor: 0.75,
